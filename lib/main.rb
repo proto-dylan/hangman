370cfcd9d7e625eb96a3bd74e9001dbfbe91d976
prot-dylan <maloney.dylan@gmail.com>
2019-11-12 17:05:06 -0500

    meow

--- lib/main.rb
+++ lib/main.rb
@@ -1,3 +1,4 @@
+require 'yaml'
 
 class Hangman 
   def initialize
@@ -9,13 +10,27 @@ class Hangman
   end
   
   def instructions
-    puts "=========================="
-    puts "=== Welcome to Hangman ==="
-    puts "=========================="
-    @board.display
-    puts "You can 6 chances to guess the right word"
-    puts "You can save at anytime by inputing 1"
-    
+    puts "      =========================="
+    puts "      === Welcome to Hangman ==="
+    puts "      =========================="
+    puts "       Guess up to 6 letters to "
+    puts "       find the secret word and "
+    puts "       save the innocent person "
+    puts "       from an archane justice! "
+    puts "      ==========================\n"
+    puts "You can save the gamestate by entering \"save\" 
+         and load last state with \"load\""
+  end
+  def play
+    @win = false
+    instructions()
+    while @win == false
+      puts "\n\n\n\n\n\n\n"
+      @board.display
+      @board.get_guess
+      @board.check_guess
+      @board.check_win
+    end 
   end
 
 end
@@ -31,12 +46,88 @@ class Board
     @remaining = 6
   end
   
+  def load_game
+    loaded_game = File.read("SavedGames/save_game.yaml")
+    game = YAML::load(loaded_game)
+
+    @guessed = game[:@guessed]
+    @secret_word = game[:@secret_word]
+    @remaining = game[:@remaining]
+    @blanks = game[:@blanks]
+    
+
+  end
+
+  def save_game
+    save = YAML::dump(self)
+    Dir.mkdir("SavedGames") unless Dir.exists?("SavedGames")
+    filename = "SavedGames/save_game.yaml"
+    File.open(filename, 'w') do |file|
+      file.puts save
+    end
+  end
+
   def display
     illustrate
     puts ""
     puts blanks.join(" ")
     puts ""
     puts "(#{@guessed.join(" ")})"
+    puts "#{@remaining} guesses remaining"
+  end
+
+  def get_guess
+    puts "\n\n\n"
+    puts "Guess a letter\n"
+    puts "input: "
+    @guess = gets.chomp.downcase
+    if @guess == "save"
+      self.save_game
+      puts "\n\n\n\n\n Game Saved!\n\n\n\n\n\n\n"
+    elsif @guess == "load"
+      self.load_game
+    end
+  end
+  def check_guess 
+    if @guess.length == 1
+      if @guessed.include?(@guess)
+        puts "You already guessed that letter"
+      elsif @secret_word.include?(@guess) 
+        @secret_word.each_with_index do |letter, index| 
+          if @guess == letter
+            @blanks[index] = @guess
+          end
+        end
+      else
+        @remaining -= 1
+      end
+      @guessed << @guess
+    end
+  end
+
+  def check_win
+    if @blanks == @secret_word
+      puts "\n\n\n\n\n\n\n"
+      puts "You won! Cut \'m down boys, done got worded right out"
+      gameover()
+    end
+    if @remaining == 0
+      puts "\n\n\n\n\n\n\n"
+      puts "You lose!  Correct word: #{@secret_word}"
+      gameover()
+    end
+  end
+
+  def gameover
+    puts "(N)ew game"
+    puts "(L)oad game"
+    response = gets.chomp
+    if response.downcase == 'n'
+      game = Hangman.new
+      game.play
+    elsif response.downcase == 'l'
+      game.load_game
+    end
   end
 
   def illustrate
@@ -122,14 +213,16 @@ class Board
       puts "|    |    /|\\    |"
       puts "|    |     |     |"
       puts "|    |    / \\    |"
-      puts "|   /|\\         |"   
+      puts "|   /|\\          |"   
       puts "| _/_|_\\ ____    |"
       puts "|________________|" 
+      puts" CORRECT ANSWER: #{@secret_word.join(" ")}"
     end
   end  
 end
 
 
 game = Hangman.new
-game.instructions
+game.play
+
 
